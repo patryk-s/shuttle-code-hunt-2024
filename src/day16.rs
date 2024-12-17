3a1b2cf9348cbd15a4cdb022fea13e80edeb504a
patryk-s <patryk.sidzina@gmail.com>
2024-12-17 19:59:19 +0100

    day16: completed

--- src/day16.rs
+++ src/day16.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use axum::{
     http::StatusCode,
     response::IntoResponse,
@@ -5,16 +7,20 @@ use axum::{
     Json, Router,
 };
 use axum_extra::extract::{cookie::Cookie, CookieJar};
-use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use jsonwebtoken::{
+    decode, decode_header, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation,
+};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 const KEY: &[u8] = b"super_secret";
+const PUBKEY_PEM: &[u8] = include_bytes!("../assets/day16_santa_public_key.pem");
 
 pub fn router() -> Router {
     Router::new()
         .route("/16/wrap", post(wrap))
         .route("/16/unwrap", get(unwrap))
+        .route("/16/decode", post(decode_handler))
 }
 
 #[axum::debug_handler]
@@ -56,8 +62,42 @@ async fn unwrap(jar: CookieJar) -> impl IntoResponse {
         }
     };
 
-    // let data: Value = serde_json::from_str(&claim.payload).unwrap();
     Json(claim.payload).into_response()
+}
+
+#[axum::debug_handler]
+async fn decode_handler(token: String) -> impl IntoResponse {
+    let header = match decode_header(&token) {
+        Ok(h) => h,
+        Err(e) => {
+            eprintln!("problem with decoding JWT header: {e:?}");
+            return StatusCode::BAD_REQUEST.into_response();
+        }
+    };
+
+    let Ok(key) = DecodingKey::from_rsa_pem(PUBKEY_PEM) else {
+        eprintln!("problem with decoding RSA PEM key");
+        return StatusCode::BAD_REQUEST.into_response();
+    };
+
+    let mut validation = Validation::new(header.alg);
+    // `exp` claim is required by default -- disable that requirement
+    validation.required_spec_claims = HashSet::new();
+    validation.validate_exp = false;
+
+    let claim = match decode::<Value>(&token, &key, &validation) {
+        Ok(t) => t.claims,
+        Err(e) if e.kind() == &jsonwebtoken::errors::ErrorKind::InvalidSignature => {
+            eprintln!("invalid JWT signature");
+            return StatusCode::UNAUTHORIZED.into_response();
+        }
+        Err(e) => {
+            eprintln!("problem with decoding JWT token: {e:?}");
+            return StatusCode::BAD_REQUEST.into_response();
+        }
+    };
+
+    Json(claim).into_response()
 }
 
 #[derive(Debug, Serialize, Deserialize)]
