7d0fb9085438765e6660ccc8b33be0ed355d938a
patryk-s <patryk.sidzina@gmail.com>
2024-12-30 19:51:46 +0100

    Day 23: completed

--- src/day23.rs
+++ src/day23.rs
@@ -1,11 +1,22 @@
-use axum::{extract::Path, http::StatusCode, response::IntoResponse, routing::get, Router};
-use html_escape::{encode_safe, encode_text};
+use std::fmt::Write;
+
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    response::{IntoResponse, Result},
+    routing::{get, post},
+    Router,
+};
+use axum_extra::extract::Multipart;
+use html_escape::encode_safe;
+use serde::Deserialize;
 
 pub fn router() -> Router {
     Router::new()
         .route("/23/star", get(star))
         .route("/23/present/:color", get(present))
         .route("/23/ornament/:state/:n", get(ornament))
+        .route("/23/lockfile", post(lockfile))
 }
 
 #[axum::debug_handler]
@@ -45,4 +56,70 @@ async fn ornament(Path((state, n)): Path<(String, String)>) -> impl IntoResponse
         r#"<div class="{class}" id="ornament{n}" hx-trigger="load delay:2s once" hx-get="/23/ornament/{next_state}/{n}" hx-swap="outerHTML"></div>"#
     )
     .into_response()
+}
+
+#[derive(Debug, Deserialize)]
+struct Lockfile {
+    package: Vec<Package>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Package {
+    checksum: Option<String>,
+}
+
+#[axum::debug_handler]
+async fn lockfile(mut multipart: Multipart) -> Result<String, StatusCode> {
+    let mut res = String::new();
+    while let Some(field) = multipart.next_field().await.map_err(|e| {
+        eprintln!("Problem with multipart: {e:?}");
+        StatusCode::BAD_REQUEST
+    })? {
+        let name = field.name().ok_or(StatusCode::BAD_REQUEST)?.to_string();
+        if name != "lockfile" {
+            return Err(StatusCode::BAD_REQUEST);
+        }
+        let data = field.text().await.map_err(|_| StatusCode::BAD_REQUEST)?;
+        eprintln!("Length of `{}` is {} bytes", name, data.len());
+        // dbg!(&data);
+
+        let lockfile: Lockfile = toml::from_str(&data).map_err(|e| {
+            eprintln!("Problem with lockfile: {e:?}");
+            StatusCode::BAD_REQUEST
+        })?;
+        // dbg!(&lockfile);
+        if lockfile.package.is_empty() {
+            eprintln!("ERROR: Empty lockfile package");
+            return Err(StatusCode::BAD_REQUEST);
+        }
+        for package in lockfile.package {
+            // skip missing package checksum
+            let checksum = match package.checksum {
+                Some(c) => c,
+                None => continue,
+            };
+            // dbg!(&checksum);
+            // first 6 bytes for the color
+            let color = checksum.get(0..6).ok_or(StatusCode::UNPROCESSABLE_ENTITY)?;
+            let color =
+                u32::from_str_radix(color, 16).map_err(|_| StatusCode::UNPROCESSABLE_ENTITY)?;
+            // next 2 bytes for top
+            let top = checksum.get(6..8).ok_or(StatusCode::UNPROCESSABLE_ENTITY)?;
+            let top = u8::from_str_radix(top, 16).map_err(|_| StatusCode::UNPROCESSABLE_ENTITY)?;
+            // next 2 bytes for left
+            let left = checksum
+                .get(8..10)
+                .ok_or(StatusCode::UNPROCESSABLE_ENTITY)?;
+            let left =
+                u8::from_str_radix(left, 16).map_err(|_| StatusCode::UNPROCESSABLE_ENTITY)?;
+            // dbg!(&color);
+            writeln!(
+                res,
+                r#"<div style="background-color:#{color:06x};top:{top}px;left:{left}px;"></div>"#
+            )
+            .unwrap();
+        }
+    }
+    // dbg!(&res);
+    Ok(res)
 }
