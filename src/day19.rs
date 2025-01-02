905d00dd626a8562a2b880bcc5c29362e54d252c
patryk-s <patryk.sidzina@gmail.com>
2025-01-02 01:54:22 +0100

    Day 19: completed

--- src/day19.rs
+++ src/day19.rs
@@ -1,21 +1,36 @@
+use std::{collections::HashMap, ops::Not, sync::Arc};
+
 use axum::{
-    extract::{Path, State},
+    extract::{Path, Query, State},
     http::StatusCode,
     response::IntoResponse,
     routing::{delete, get, post, put},
     Json, Router,
 };
+use rand::distributions::{Alphanumeric, DistString};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use sqlx::{
     prelude::FromRow,
-    query, query_as,
+    query, query_as, query_scalar,
     types::chrono::{DateTime, Utc},
     PgPool,
 };
+use tokio::sync::Mutex;
 use uuid::Uuid;
 
+#[derive(Clone)]
+struct Data {
+    db: PgPool,
+    pagination: Arc<Mutex<HashMap<String, i64>>>,
+}
+
 pub fn router(pool: PgPool) -> Router {
+    let data = Data {
+        db: pool,
+        pagination: Arc::new(Mutex::new(HashMap::new())),
+    };
+
     Router::new()
         .route("/19/reset", post(reset))
         .route("/19/cite/:id", get(get_id))
@@ -23,19 +38,19 @@ pub fn router(pool: PgPool) -> Router {
         .route("/19/undo/:id", put(update))
         .route("/19/draft", post(create))
         .route("/19/remove/:id", delete(remove))
-        .with_state(pool)
+        .with_state(data)
 }
 
 #[axum::debug_handler]
-async fn reset(State(db): State<PgPool>) -> impl IntoResponse {
+async fn reset(State(data): State<Data>) -> impl IntoResponse {
     query!(r#"DELETE FROM quotes"#)
-        .execute(&db)
+        .execute(&data.db)
         .await
         .expect("deleting quotes");
 }
 
 #[axum::debug_handler]
-async fn create(State(db): State<PgPool>, Json(draft): Json<Draft>) -> impl IntoResponse {
+async fn create(State(data): State<Data>, Json(draft): Json<Draft>) -> impl IntoResponse {
     let id = Uuid::new_v4();
     match query_as!(
         Quote,
@@ -44,7 +59,7 @@ async fn create(State(db): State<PgPool>, Json(draft): Json<Draft>) -> impl Into
         draft.author,
         draft.quote,
     )
-        .fetch_one(&db)
+        .fetch_one(&data.db)
         .await
     {
         Ok(quote) => (StatusCode::CREATED, Json(quote)).into_response(),
@@ -56,13 +71,13 @@ async fn create(State(db): State<PgPool>, Json(draft): Json<Draft>) -> impl Into
 }
 
 #[axum::debug_handler]
-async fn remove(State(db): State<PgPool>, Path(id): Path<Uuid>) -> impl IntoResponse {
+async fn remove(State(data): State<Data>, Path(id): Path<Uuid>) -> impl IntoResponse {
     match query_as!(
         Quote,
         r#"DELETE FROM quotes WHERE id = $1 RETURNING id, author, quote, created_at, version"#,
         id,
     )
-    .fetch_one(&db)
+    .fetch_one(&data.db)
     .await
     {
         Ok(quote) => (StatusCode::OK, Json(quote)).into_response(),
@@ -75,7 +90,7 @@ async fn remove(State(db): State<PgPool>, Path(id): Path<Uuid>) -> impl IntoResp
 
 #[axum::debug_handler]
 async fn update(
-    State(db): State<PgPool>,
+    State(data): State<Data>,
     Path(id): Path<Uuid>,
     Json(draft): Json<Draft>,
 ) -> impl IntoResponse {
@@ -86,7 +101,7 @@ async fn update(
         draft.quote,
         id,
     )
-        .fetch_one(&db)
+        .fetch_one(&data.db)
         .await
     {
         Ok(quote) => (StatusCode::OK, Json(quote)).into_response(),
@@ -98,13 +113,13 @@ async fn update(
 }
 
 #[axum::debug_handler]
-async fn get_id(State(db): State<PgPool>, Path(id): Path<Uuid>) -> impl IntoResponse {
+async fn get_id(State(data): State<Data>, Path(id): Path<Uuid>) -> impl IntoResponse {
     match query_as!(
         Quote,
         r#"SELECT id, author, quote, created_at, version FROM quotes where id = $1"#,
         id,
     )
-    .fetch_one(&db)
+    .fetch_one(&data.db)
     .await
     {
         Ok(quote) => (StatusCode::OK, Json(quote)).into_response(),
@@ -113,20 +128,61 @@ async fn get_id(State(db): State<PgPool>, Path(id): Path<Uuid>) -> impl IntoResp
 }
 
 #[axum::debug_handler]
-async fn list(State(db): State<PgPool>) -> impl IntoResponse {
+async fn list(State(data): State<Data>, Query(params): Query<ListParams>) -> impl IntoResponse {
+    let limit = 3;
+    let num_quotes = match query_scalar!(r#"SELECT count(*) FROM quotes"#,)
+        .fetch_one(&data.db)
+        .await
+    {
+        Ok(quotes) => quotes.unwrap(),
+        Err(_) => return StatusCode::NOT_FOUND.into_response(),
+    };
+
+    let mut next_token = params.token.clone();
+
+    let page = if num_quotes > limit {
+        match params.token {
+            Some(token) => {
+                if data.pagination.lock().await.contains_key(&token).not() {
+                    return StatusCode::BAD_REQUEST.into_response();
+                }
+                data.pagination
+                    .lock()
+                    .await
+                    .entry(token)
+                    .and_modify(|e| *e += 1)
+                    .or_default()
+                    .to_owned()
+            }
+            None => {
+                let new_token = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
+                next_token = Some(new_token.clone());
+                data.pagination.lock().await.insert(new_token, 1);
+                1
+            }
+        }
+    } else {
+        1
+    };
+
+    let offset = (page - 1) * limit;
     let quotes = match query_as!(
         Quote,
-        r#"SELECT id, author, quote, created_at, version FROM quotes ORDER BY created_at"#,
+        r#"SELECT id, author, quote, created_at, version FROM quotes ORDER BY created_at LIMIT $1 OFFSET $2"#,
+        limit,
+        offset,
     )
-    .fetch_all(&db)
+    .fetch_all(&data.db)
     .await
     {
         Ok(quotes) => quotes,
         Err(_) => return StatusCode::NOT_FOUND.into_response(),
     };
 
-    let page = 1;
-    let next_token = if quotes.len() > 3 { Some("foo") } else { None };
+    // at last page
+    if page * limit >= num_quotes {
+        next_token = None;
+    }
 
     (
         StatusCode::OK,
@@ -148,4 +204,9 @@ struct Quote {
 struct Draft {
     author: String,
     quote: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct ListParams {
+    token: Option<String>,
 }
